55edf3a968d0fd4e54f1fb447c242495a0b2c150
devsobirov <devsobirov@gmail.com>
2023-08-10 06:40:01 +0500

    localization files updated

---
+++ lang/en/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+return array (
+  'email' => 'info@sanoatuquv.uz',
+  'phone' => '+998 79 222 77 00',
+  'phone1' => '+998 79 222 11 00',
+  'phone2' => '+998 79 224 51 38',
+  'du-shan' => 'Mon-Sat',
+);
---
+++ lang/uz/settings.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+  'phone' => '+998 79 222 77 00',
+  'phone1' => '+998 79 222 11 00',
+  'phone2' => '+998 79 224 51 38',
+  'email' => 'info@sanoatuquv.uz',
+  'adress' => 'NAVOIY VILOYATI, NAVOIY SHAHAR, Navoiy 210100',
+  'du-shan' => 'Du-Shan',
+  'yak' => 'Yak',
+  'footer-text' => 'O\'quv markazimiz 2008 yildan beri o\'z faoliyatini olib bormoqda. Shu davr mobaynida 2000 dan ortiq ishsiz odamni kasb egalari bo\'lishga yordam berib kelmoqda.',
+  'facebook' => '#',
+  'twitter' => '#',
+  'instagram' => '#',
+  'telegram' => '#',
+);
